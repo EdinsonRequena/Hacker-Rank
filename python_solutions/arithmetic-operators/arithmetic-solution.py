793d41d8d5ac5766dfa788c8ff0dd328a06151a4
EdinsonRequena <edinsonrequena@protonmail.com>
2020-08-11 02:17:20 -0400

    adding arimethmtic solution

---
+++ python_solutions/arithmetic-operators/arithmetic-solution.py
@@ -0,0 +1,14 @@
+def main(a, b):
+    suma = a + b
+    resta = a - b
+    producto = a * b
+    
+    print(suma)
+    print(resta)
+    print(producto)
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    main(a, b)
