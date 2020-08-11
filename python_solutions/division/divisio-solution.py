cd030fd82421bdc0478e4b5703b529e1d65f3f26
EdinsonRequena <edinsonrequena@protonmail.com>
2020-08-11 02:20:43 -0400

    adding division solution

---
+++ python_solutions/division/divisio-solution.py
@@ -0,0 +1,11 @@
+def main(a, b):
+    natural = a // b
+    racional = a / b
+
+    print(natural)
+    print(racional)
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    main(a,b)
